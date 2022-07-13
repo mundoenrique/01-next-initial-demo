3a5025f8f40f9f75bb14c81ab5e2d4ea1ebc18d0
epenaloza <epenaloza@novopayment.com>
2022-07-13 18:58:50 -0400

    Extrae constante del componente

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,27 +1,26 @@
-import Link from 'next/link';
 import { ActiveLink } from './ActiveLink';
 import styles from './NavBar.module.css';
 
-export function NavBar() {
-  const menuItems = [
-    {
-      text: 'Home',
-      href: '/',
-    },
-    {
-      text: 'About',
-      href: '/about',
-    },
-    {
-      text: 'Contact',
-      href: '/contac',
-    },
-    {
-      text: 'Pricing',
-      href: '/pricing',
-    },
-  ];
+const menuItems = [
+  {
+    text: 'Home',
+    href: '/',
+  },
+  {
+    text: 'About',
+    href: '/about',
+  },
+  {
+    text: 'Contact',
+    href: '/contac',
+  },
+  {
+    text: 'Pricing',
+    href: '/pricing',
+  },
+];
 
+export function NavBar() {
   return (
     <nav className={styles['menu-container']}>
       {menuItems.map((item) => (
